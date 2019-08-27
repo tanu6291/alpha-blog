59dddd9a9dc95c67f90cd8969aa06b85af24a8fd
Rails Udemy course-Tanu <railsudemycoursetanu@gmail.com>
2019-08-27 21:22:50 +0530

    create new template action and route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
 
 	# get 'pages/about', to: 'pages#about'
 	get 'about', to: 'pages#about'
+
+	resources :articles
+	# this gives the edit, create, update, show,index,delete article path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
