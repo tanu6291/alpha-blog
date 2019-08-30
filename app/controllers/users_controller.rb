c2f0c4c41de897d3d1aed4a95919cd395d6d6624
Rails Udemy course-Tanu <railsudemycoursetanu@gmail.com>
2019-08-30 21:18:00 +0530

    Add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+	def index
+	 @users = User.all
+	end
+
 	def new
 		@user = User.new
 	end
@@ -34,10 +38,9 @@ class UsersController < ApplicationController
 
 
 	private
-	def user_params
-		params.require(:user).permit(:username, :email, :password)
-	end
-
+		def user_params
+			params.require(:user).permit(:username, :email, :password)
+		end
 end
 
 
