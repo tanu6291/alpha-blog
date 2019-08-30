34a40ddd904b9813d7e0493dba0d69b46bcfbe96
Rails Udemy course-Tanu <railsudemycoursetanu@gmail.com>
2019-08-30 15:36:39 +0530

    Add assocciation of user and Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
+	belongs_to :user
 	validates :title, presence: true, length: { minimum: 3, maximum: 100 }
 	validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+	validates :user_id, presence: true
 
 end
