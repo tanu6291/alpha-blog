d649d9653575d4c133fb0c0c61eab323333556d5
Rails Udemy course-Tanu <railsudemycoursetanu@gmail.com>
2019-08-31 15:22:11 +0530

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
 	get 'signup', to: 'users#new'
 	resources :users, except: [:new]
+
+	get 'login', to: 'sessions#new'
+	post 'login', to: 'sessions#create'
+	delete 'logout', to: 'sessions#destroy'
 	# this gives the edit, create, update, show,index,delete article path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
